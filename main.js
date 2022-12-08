891d2c02bf2489d141283c27e1ed635995ee4188
hebamesluh1 <hebamesluh@gmail.com>
2022-12-08 09:59:12 +0200

    edit reset btn alghorithm

--- main.js
+++ main.js
@@ -6,16 +6,18 @@ let stopCounter = document.querySelector('.stop-watch-counter');
 let startBtn =document.querySelector('#start');
 let resetBtn = document.querySelector('#reset');
 let timeline = document.querySelector('.timeline');
+let input = document.querySelector('#timer-input');
+let timerPart = document.querySelector('.timer-counter-displsy');
 
 //time stopwatch
 let sec = document.querySelector('.second');
 let m_sec=document.querySelector('.m-second');
 let min=document.querySelector('.minute');
 let hr=document.querySelector('.hour');
-let m_second=00;
-let second=00;
-let minute = 00;
-let hours = 00;
+let m_second=0;
+let second=0;
+let minute = 0;
+let hours = 0;
 let interval;
 let timeOf;
 
@@ -34,6 +36,12 @@ timer.addEventListener('click',()=>{
     timer.classList.add('active');
     stopCounter.style.display='none';
     timerCounter.style.display='block';
+
+
+    //remove timer input 
+    if(input.style.display == 'block'){
+        input.style.display = 'none'
+    }
 
 });
 
@@ -101,11 +109,25 @@ function startTimer(){
     }
 }
 
+//when click on the timer counter user can input 
+// timerCounter.addEventListener('click',()=>{
+//     input.style.display='block';
 
-
+// });
 
 //counterDown 
-const startMin = 5;
+let startMin = 5;
+// timerCounter.addEventListener('click',()=>{
+//     input.style.display='block';
+//     timerPart.style.display='none';
+// input.addEventListener('input',(e)=>{
+//     startMin = e.target.value;
+// });
+// });
+// input.style.display='none';
+// timerPart.style.display='block';
+// timeMin.innerHTML = startMin;
+
 let time = startMin *60;
 const timeMin = document.querySelector('.time_minutes');
 const timeSec = document.querySelector('.time_sec');
@@ -131,11 +153,12 @@ resetBtn.addEventListener('click',()=>{
 
     //reset stopwatch counter 
         clearInterval(interval);
-        let m_second=00;
-        let second=00;
-        let minute=00;
-        let hours =00;
-        m_sec.innerHTML='0'+m_second;
+            m_second=0;
+            second=0;
+        // console.log('done')
+            minute=00;
+            hours =00;
+        m_sec.innerHTML=`0 ${m_second}`;
         sec.innerHTML=second;
         min.innerHTML=minute;
         hr.innerHTML=hours;
@@ -144,11 +167,14 @@ resetBtn.addEventListener('click',()=>{
 
     //timeline reset
     timeline.style.width = 0;
-    //reset timer counter 
 
-    // clearInterval(timeOf);
-    //     timeMin.innerHTML = 5;
-    //     timeSec.innerHTML = '0'+second;
+
+
+    //reset timer counter 
+    clearInterval(timeOf);
+        startMin=5;
+        timeMin.innerHTML = startMin;
+        timeSec.innerHTML = second;
 });
 
 
@@ -157,3 +183,18 @@ resetBtn.addEventListener('click',()=>{
     expand.addEventListener('click',()=>{
         document.querySelector('main').classList.toggle('expand');
     });
+
+
+//showing input when clicking on div 
+// timerCounter.addEventListener('click',()=>{
+//     // console.log('timer click');
+//     input.style.display='block';
+//     timerPart.style.display='none';
+// });
+
+
+
+// timerCounter.addEventListener('load',()=>{
+//     input.style.display='none';
+//     timerPart.style.display='block';
+// })
