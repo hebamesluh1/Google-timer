fad8d379c9f71c6c88e27893fd8bc0154e65c377
hebamesluh1 <hebamesluh@gmail.com>
2022-12-10 07:14:47 +0200

    adding logic for input timer

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-// variables 
+/////////////////////////variables//////////////////////////////////////
 let timer = document.querySelector('.timer');
 let timerCounter = document.querySelector('#timer-counter')
 let stopwatch = document.querySelector('.stopwatch');
@@ -6,25 +6,23 @@ let stopCounter = document.querySelector('.stop-watch-counter');
 let startBtn =document.querySelector('#start');
 let resetBtn = document.querySelector('#reset');
 let timeline = document.querySelector('.timeline');
-let input = document.querySelector('#timer-input');
 let timerPart = document.querySelector('.timer-counter-displsy');
-
-//time stopwatch
+const timerInput = document.querySelector('.timeInput');
+const timerInputHidden = document.querySelector('#timerInputHidden');
+const hoursInput = document.querySelector('.hours');
+const minutesInput = document.querySelector('.minutes');
+const secondsInput = document.querySelector('.seconds');
 let sec = document.querySelector('.second');
 let m_sec=document.querySelector('.m-second');
 let min=document.querySelector('.minute');
 let hr=document.querySelector('.hour');
-let m_second=0;
-let second=0;
-let minute = 0;
-let hours = 0;
-let interval;
-let timeOf;
 
 
 
 
-// timer and stopwatch interface
+
+/////////////////////////////// timer and stopwatch //////////////////////////////
+//displays stopwatch and timer
 stopwatch.addEventListener('click',()=>{
     stopwatch.classList.add('active');
     timer.classList.remove('active');
@@ -36,41 +34,15 @@ timer.addEventListener('click',()=>{
     timer.classList.add('active');
     stopCounter.style.display='none';
     timerCounter.style.display='block';
-
-
-    //remove timer input 
-    if(input.style.display == 'block'){
-        input.style.display = 'none'
-    }
-
 });
 
-
-
-//when click on start button 
-startBtn.addEventListener('click',()=>{
-    if(startBtn.textContent === 'start'){
-        //stopwatch work
-        if(stopCounter.style.display == 'block' ){
-            clearInterval(interval);
-            interval = setInterval(startTimer, 10);
-        }else{
-            clearInterval(timeOf);
-            timeOf = setInterval(updateCountdown,1000);
-        }
-            startBtn.textContent='stop';
-    }else{
-        clearInterval(interval);
-        clearInterval(timeOf);
-        startBtn.textContent='start';
-    }
-
-});
-
-
-
-
-//start time function
+//stop watch function
+let m_second=0;
+let second=0;
+let minute = 0;
+let hours = 0;
+let interval;
+let timeOf;
 function startTimer(){
     m_second++;
     if(m_second<=9){
@@ -109,38 +81,22 @@ function startTimer(){
     }
 }
 
-//when click on the timer counter user can input 
-// timerCounter.addEventListener('click',()=>{
-//     input.style.display='block';
-
-// });
-
-//counterDown 
-let startMin = 5;
-// timerCounter.addEventListener('click',()=>{
-//     input.style.display='block';
-//     timerPart.style.display='none';
-// input.addEventListener('input',(e)=>{
-//     startMin = e.target.value;
-// });
-// });
-// input.style.display='none';
-// timerPart.style.display='block';
-// timeMin.innerHTML = startMin;
-
+//counterDown function 
+const startMin = 5;
 let time = startMin *60;
+let secnd;
 const timeMin = document.querySelector('.time_minutes');
 const timeSec = document.querySelector('.time_sec');
 function updateCountdown(){
     const minutes = Math.floor(time/60);
-    let sec =time% 60;
-    sec= sec<10?'0'+sec:sec;
+    secnd =time% 60;
+    secnd= secnd<10?'0'+secnd:secnd;
     timeMin.innerHTML = minutes;
-    timeSec.innerHTML = sec;
+    timeSec.innerHTML = secnd;
     time--;
     let timeLineWidth = timeline.offsetWidth;
     timeLineWidth = timeLineWidth+2.03;
-    timeline.style.width = `${timeLineWidth}px`
+    timeline.style.width = `${timeLineWidth}px`;
 }
 
 
@@ -148,16 +104,135 @@ function updateCountdown(){
 
 
 
-// when click on reset button
+
+
+
+////////////////////////timer input action ////////////////////////////////
+//when click on timer counter display timer
+timerCounter.addEventListener('click',()=>{
+        timerInput.style.display='block';
+        timerPart.style.display='none';
+        timerInputHidden.style.display='block';
+})
+//when focus on input change color of label "timer input hidden"
+timerInput.addEventListener('click', (e) => {
+    timerInputHidden.focus();
+})
+timerInputHidden.addEventListener('focus', ()=>{
+    timerInput.classList.add('active')
+    timerInput.querySelectorAll('.unit').forEach(span => {
+        span.classList.add('pure')
+    })
+})
+// allow user to inter time 
+timerInputHidden.addEventListener('keyup', (e)=>{
+        handelTime(e.target.value);
+})
+//change value of input number based on user input
+timerInputHidden.addEventListener('change', (e)=>{
+    handelTime(e.target.value)
+})
+let hrs, minu, seco;
+const secInputArrElem = secondsInput.querySelector('.num');
+const minInputArrElem = minutesInput.querySelector('.num');
+const horsInputArrElem = hoursInput.querySelector('.num');
+function handelTime (timeStr) {
+    {
+        let time = [];
+        if(timeStr.length % 2 != 0){
+            time.push(`0${timeStr[0]}`);
+        for(let i =1; i < timeStr.length; i+=2){
+                time.push(`${timeStr[i]}${timeStr[i+1]}`)
+        }
+        }
+        else {
+            for(let i =0; i < timeStr.length; i+=2){
+                    time.push(`${timeStr[i]}${timeStr[i+1]}`)
+            }
+        }
+        console.log(time);
+        if(time.length === 3)
+        {
+            hrs = time[0].length === 2 ? time[0] : `0${time[0]}`;
+            minu = time[1].length === 2 ? time[1] : `0${time[1]}`;
+            seco = time[2].length === 2 ? time[2] : `0${time[2]}`;
+        }
+        else if(time.length === 2) {
+            hrs = undefined;
+            minu = time[0].length === 2 ? time[0] : `0${time[0]}`;
+            seco = time[1].length === 2 ? time[1] : `0${time[1]}`;
+        }
+        else if (time.length === 1){
+            hrs = undefined;
+            minu = undefined;
+            seco = time[0].length === 2 ? time[0] : `0${time[0]}`;
+        }
+        else{
+            hrs = undefined;
+            minu = undefined;
+            seco = undefined;
+        }
+        hrs ? hoursInput.classList.remove('pure') : hoursInput.classList.add('pure')
+        minu ? minutesInput.classList.remove('pure') : minutesInput.classList.add('pure')
+        seco ? secondsInput.classList.remove('pure') : secondsInput.classList.add('pure')
+    
+        if(seco)
+            secInputArrElem.textContent = seco;
+    
+        if(minu)
+            minInputArrElem.textContent = minu;
+    
+        if(hrs)
+            horsInputArrElem.textContent = hrs;
+        }}
+
+
+
+////////////////////////timer input action countdown////////////////////////////////
+
+
+
+
+
+
+
+
+
+
+
+
+/////////////////////////// start button and stop ////////////////////////////////////////////////
+startBtn.addEventListener('click',()=>{
+    if(startBtn.textContent === 'start'){
+        //stopwatch work
+        if(stopCounter.style.display == 'block' ){
+            clearInterval(interval);
+            interval = setInterval(startTimer, 10);
+        }else{
+            clearInterval(timeOf);
+            timeOf = setInterval(updateCountdown,1000);
+        }
+            startBtn.textContent='stop';
+    }else{
+        clearInterval(interval);
+        clearInterval(timeOf);
+        startBtn.textContent='start';
+    }
+
+});
+
+
+
+
+////////////////////////////////////// reset button //////////////////////////////////////
 resetBtn.addEventListener('click',()=>{
 
-    //reset stopwatch counter 
+    //stopwatch counter reset
         clearInterval(interval);
-            m_second=0;
-            second=0;
-        // console.log('done')
-            minute=00;
-            hours =00;
+        m_second=0;
+        second=0;
+        minute=00;
+        hours =00;
         m_sec.innerHTML=`0 ${m_second}`;
         sec.innerHTML=second;
         min.innerHTML=minute;
@@ -168,33 +243,36 @@ resetBtn.addEventListener('click',()=>{
     //timeline reset
     timeline.style.width = 0;
 
-
-
-    //reset timer counter 
+    // timer counter reset
     clearInterval(timeOf);
-        startMin=5;
+        // startMin=5;
+        // secnd=0;
         timeMin.innerHTML = startMin;
         timeSec.innerHTML = second;
+
+
+    //input timer reset
+    hrs=0;
+    minu=0;
+    seco=0;
+    secInputArrElem.textContent = `0${seco}`;
+    minInputArrElem.textContent = `0${minu}`;
+    horsInputArrElem.textContent = `0${hrs}`;
 });
 
 
-//expand window
+
+
+
+
+
+
+
+
+
+////////////////////////expand window//////////////////////////////////
     let expand = document.querySelector('.fa-expand');
     expand.addEventListener('click',()=>{
         document.querySelector('main').classList.toggle('expand');
     });
 
-
-//showing input when clicking on div 
-// timerCounter.addEventListener('click',()=>{
-//     // console.log('timer click');
-//     input.style.display='block';
-//     timerPart.style.display='none';
-// });
-
-
-
-// timerCounter.addEventListener('load',()=>{
-//     input.style.display='none';
-//     timerPart.style.display='block';
-// })
